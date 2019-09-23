9d799b0e19279eabf83f7c5a8a1837d3d970d983
isauregarrigue <55700772+isauregarrigue@users.noreply.github.com>
2019-09-23 17:13:01 +0200

    Add files via upload

---
+++ Atelier1.R
@@ -0,0 +1,23 @@
+2 + 3
+8 - 12
+3.03-sqrt(2)
+x <- 2
+x
+y<-seq(from=0,to=10^6,by=1)
+y
+y[1000]
+tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+tailles
+mean(tailles)#moyenne
+var(tailles)#variance
+sd(tailles)#écart-type
+summary(tailles)#resume
+tailles^2
+sin(tailles)
+poids <- c(86, 74, 83, 50, 78, 66, 66, 51, 50, 55)
+length(tailles)
+length(poids)
+tailles.m<-tailles/100
+tailles.m
+imc<-poids/tailles.m^2
+imc
